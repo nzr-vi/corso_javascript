9733c6f725977621271c7db859902cf7443ced77
nzrv <96348033+nzr-vi@users.noreply.github.com>
2022-02-03 09:59:22 +0100

    leggi from url

--- zz_es/e220203am/leggiFromURL/script.js
+++ zz_es/e220203am/leggiFromURL/script.js
@@ -1,12 +1,38 @@
 "use strict";
 
-//file: 
+//file:
 
+function onFetchRequest(event){
+    event.stopPropagation();
+    const inputNode = document.getElementById("input_url");
+    const url = inputNode.value;
+    /*
+        XMLHttpRequest
+    */
+
+    fetch(url)
+    .then(response => {
+        if (!response.ok) 
+            throw new Error('Responce was not ok');
+        return response.text();
+    })
+    .then(text => {
+        //console.log(text);
+        const myDiv = document.getElementById("output_codice");
+        myDiv.innerHTML = text;
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+        alert("something went wrong");
+    });
+
+}
 
 window.addEventListener(
     'DOMContentLoaded', 
     function(event){
-
+        const fetchBtn = document.getElementById("btn_fetch_url");
+        fetchBtn.addEventListener('click',onFetchRequest);
     }
 );
 
