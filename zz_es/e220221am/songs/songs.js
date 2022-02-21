0a139811275215d2ff5836aec4530fab4db7b788
nzrv <96348033+nzr-vi@users.noreply.github.com>
2022-02-21 12:48:27 +0100

    1st edit

--- zz_es/e220221am/songs/songs.js
+++ zz_es/e220221am/songs/songs.js
@@ -12,11 +12,35 @@ PREREQUISITI: la app deve essere responsive e single page
 6. TODO implementare la Add Song (anche se solo simulata con fake response)
 7. TODO implementare la Edit Song (anche se solo simulata con fake response)
 8. TODO implementare la Delete Song (anche se solo simulata con fake response)
+
+id input type text but read only
+
+insert a form inside the modal to allow the edit of the 4 (-1) elements
+put the datas inside the form, (handluing the 2 cases, add song and edit in case
+	the id has a value or not)
 */
+
+
+
+let songModal;
+
+function writeSong(event){
+
+	let originator = event.currentTarget;
+	let song_id = originator.getAttribute('data-song-id');
+	songModal.show();
+}
 
 window.addEventListener(
 	'DOMContentLoaded', 
 	function(event){
 
+		songModal = new bootstrap.Modal(document.getElementById('exampleModal'), {});
+		//songModal.show();
+		let btns_write_song = document.querySelectorAll('.write-song');
+		btns_write_song.forEach(element => {
+			//console.log(element);
+			element.addEventListener('click',writeSong);
+		});
 	}
 );
